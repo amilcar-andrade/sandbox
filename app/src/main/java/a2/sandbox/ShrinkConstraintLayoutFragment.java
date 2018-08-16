db27d76ce4c511baa611edbcef15d4b265a871ae
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2018-08-16 12:36:21 -0700

    Adds custom transition to the ShrinkConstraintLayoutFragment implementation

--- app/src/main/java/a2/sandbox/ShrinkConstraintLayoutFragment.java
+++ app/src/main/java/a2/sandbox/ShrinkConstraintLayoutFragment.java
@@ -6,34 +6,47 @@ import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.transition.ChangeBounds;
+import android.transition.Fade;
+import android.transition.Transition;
 import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-public class ShrinkConstraintLayoutFragment extends Fragment implements View.OnClickListener {
+public class ShrinkConstraintLayoutFragment extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     public static final String TAG = "ShrinkConstraintLayoutFragment";
 
-    private ConstraintLayout root;
+    private ConstraintLayout constraintLayout;
 
     private ConstraintSet idleSet = new ConstraintSet();
 
     private ConstraintSet triggeredSet = new ConstraintSet();
 
+    private boolean useAutoTransition;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        root = (ConstraintLayout) inflater.inflate(R.layout.fragment_shrinkg_constraint_layout, container, false);
-        View helpButton = root.findViewById(R.id.help_button);
-        View okButton = root.findViewById(R.id.ok_button);
+        View root = inflater.inflate(R.layout.fragment_shrinkg_constraint_layout, container, false);
+        CheckBox checkBox = root.findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(this);
+        constraintLayout = root.findViewById(R.id.constraint_layout);
+        View helpButton = constraintLayout.findViewById(R.id.help_button);
+        View okButton = constraintLayout.findViewById(R.id.ok_button);
         helpButton.setOnClickListener(this);
         okButton.setOnClickListener(this);
 
         // Persist the previous constraints so we can switch between both states
-        idleSet.clone(root);
+        idleSet.clone(constraintLayout);
 
-        triggeredSet.clone(root);
+        triggeredSet.clone(constraintLayout);
         triggeredSet.setVisibility(R.id.title, View.GONE);
         triggeredSet.setVisibility(R.id.instructions, View.GONE);
         triggeredSet.setVisibility(R.id.help_button, View.GONE);
@@ -48,13 +61,46 @@ public class ShrinkConstraintLayoutFragment extends Fragment implements View.OnC
 
     @Override
     public void onClick(View v) {
-        TransitionManager.beginDelayedTransition(root);
         switch (v.getId()) {
             case R.id.help_button:
-                triggeredSet.applyTo(root);
+                beginDelayedTransition(false);
+                triggeredSet.applyTo(constraintLayout);
                 break;
             case R.id.ok_button:
-                idleSet.applyTo(root);
+                beginDelayedTransition(true);
+                idleSet.applyTo(constraintLayout);
         }
+    }
+
+    private void beginDelayedTransition(boolean playReverse) {
+        if (useAutoTransition) {
+            TransitionManager.beginDelayedTransition(constraintLayout);
+            return;
+        }
+        TransitionManager.beginDelayedTransition(constraintLayout, createTransition(playReverse));
+    }
+
+    // LATER: Create the transition only once
+    private Transition createTransition(boolean playReverse) {
+       return new TransitionSet()
+                .addTransition(new Fade()
+                        .addTarget(R.id.title)
+                        .addTarget(R.id.instructions)
+                        .setDuration(playReverse ? 400 : 100))
+                .addTransition(new Fade()
+                        .addTarget(R.id.help_button)
+                        .addTarget(R.id.ok_button)
+                        .setDuration(playReverse ? 100 : 400))
+                .addTransition(new ChangeBounds()
+                        .setDuration(playReverse ? 100 : 400)
+                        .setInterpolator(
+                                playReverse ?
+                                new FastOutLinearInInterpolator() :
+                                new FastOutSlowInInterpolator()));
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        useAutoTransition = isChecked;
     }
 }
