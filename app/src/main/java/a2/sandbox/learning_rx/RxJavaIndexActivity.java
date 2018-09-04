b6bcd4008fa5ce08e8c8ae97a817d1ad1f7b0525
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2018-09-04 09:39:39 -0700

    Setup for RxJava and Kotlin

--- app/src/main/java/a2/sandbox/learning_rx/RxJavaIndexActivity.java
+++ app/src/main/java/a2/sandbox/learning_rx/RxJavaIndexActivity.java
@@ -1,6 +1,16 @@
 package a2.sandbox.learning_rx;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import a2.sandbox.R;
+
 public class RxJavaIndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_rx);
+    }
 }
